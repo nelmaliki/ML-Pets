76ecf01ba6527a68824f2319cb1b27f5df20ddca
Nour Elmaliki <46309662+nelmaliki@users.noreply.github.com>
2021-04-23 15:47:09 -0400

    CSV file generator

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import utils
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+sound_csv = "noise_numbers.csv"
 def main():
-    dataset = utils.load_dataset()
-    print(dataset
-          )
+    df = None
+    if os.path.isfile(sound_csv):
+        df = pd.read_csv(sound_csv)
+    else:
+        print("Csv file of processed wav files was not found, this might take awhilez...")
+        data = utils.load_dataset()
+        noise = []
+        for i in range(0,max([len(l) for l in list(data.values())])):
+            tmp = []
+            for l in list(data.values()):
+                if i >= len(l):
+                    tmp.append(None)
+                else:
+                    tmp.append(l[i])
+            noise.append(tmp)
+        df = pd.DataFrame(noise, columns = list(data.keys()))
+        print("Dataframe created, saving to ", sound_csv)
+        df.to_csv(sound_csv)
+
+    print(df)
+    sns.heatmap(np.abs(df.corr()), annot=True, cmap="YlGnBu")
+    plt.title("Correlation Heatmap")
+    plt.show()
 if __name__ == "__main__":
     main()
